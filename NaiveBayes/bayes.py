b02124499c3f3476d7e8e36e1072d54d47d5e51f
zhaocc <450959507@qq.com>
2018-05-23 10:40:07 +0800

    add spam filter test by naive bayes.

--- NaiveBayes/bayes.py
+++ NaiveBayes/bayes.py
@@ -62,6 +62,15 @@ def setOfWords2Vec(vocablist, inputSet):
             #print ("this word is not in vocablist:%s" %vocablist[i])
     return wordVect
 
+# create bag of words model vector.
+def bagOfWords2Vec(vocablist, inputSet):
+    numberWords = len(vocablist)
+    wordVect = zeros(numberWords)
+    for i in range(numberWords):
+        if vocablist[i] in inputSet:
+            wordVect[i] += 1
+    return wordVect
+
 # classify by probability vector of every word in every class and probability of every class.
 def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
     p1 = sum(vec2Classify * p1Vec) + log(pClass1)
@@ -94,11 +103,57 @@ def testingNB():
     #print("thisDoc:", thisDoc)
     print(testEntry, " classified as ", classifyNB(thisDoc, p0V, p1V, pAb))
 
+# text parse
+def textParse(bigString):
+    import re
+    listOfTokens = re.split(r'\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+# filter spam
+def spamTest():
+    docList = []; classList = []; fullText = []
+
+    # 导入并解析邮件
+    for i in range(1, 26):
+        wordList = textParse(open("email/spam/%d.txt" % i).read())
+        #print("wordList:\n" + str(wordList))
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+
+        wordList = textParse(open("email/ham/%d.txt" % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+
+    vocbList = createVocabList(docList)
+    trainingSet = list(range(50)); testSet = []
+
+    # 创建随机训练集
+    for i in range(10):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+
+    # 创建训练集词汇向量表
+    trainMat = []; trainClass = []
+    for i in trainingSet:
+        trainMat.append(setOfWords2Vec(vocbList, docList[i]))
+        trainClass.append(classList[i])
+
+    # 计算朴素贝叶斯概率
+    p1V, p0V, pSpam = trainNB0(array(trainMat), array(trainClass))
+    #print("p0V:" + str(p0V) + "\np1V:" + str(p1V) + "\npSpam:" + str(pSpam))
+
+    # 使用上述计算出的概率计算测试集合，并统计错误率
+    errorCount = 0
+    for docIndex in testSet:
+        wordVect = setOfWords2Vec(vocbList, docList[docIndex])
+        if classifyNB(wordVect, p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+    print("the error rate is %f" % float(errorCount / len(testSet)))
+
+
 if __name__ == '__main__':
-     testingNB()
-
-
-
-
-
-
+     #testingNB()
+    spamTest()
