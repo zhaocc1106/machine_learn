0250020a66c30c2be0f03285e3f537decba90af4
zhaochaochao <zhaochaochao@baidu.com>
2018-07-04 21:14:34 +0800

    Add FP-Growth

---
+++ FP-growth/FP-growth.py
@@ -0,0 +1,128 @@
+class treeNode:
+    def __init__(self, nameValue, numOccur, parentNode):
+        self.name = nameValue               # 节点的名字
+        self.count = numOccur               # 该节点元素在所有数据集中出现的次数
+        self.parent = parentNode            # 该节点的父节点
+        self.nodeLink = None                # 用于连接相似的元素项
+        self.children = {}                  # 该节点的子节点
+
+    def inc(self, numOccur):
+        self.count += numOccur
+
+    def disp(self, ind=1):
+        """
+        将树的数据展示出来，并且通过空格来分层次
+        :param ind: 代表节点的层级，同事代表有几个空格在最开始
+        """
+        print("  " * ind, self.name, " ", self.count)
+        for child in self.children.values():
+            child.disp(ind+1)               # 子节点层级加一
+
+def loadSimpDat():
+    simpleDat = [['r', 'z', 'h', 'j', 'p'],
+                 ['z', 'y', 'x', 'w', 'v', 'u', 't', 's'],
+                 ['z'],
+                 ['r', 'x', 'n', 'o', 's'],
+                 ['y', 'r', 'x', 'z', 'q', 't', 'p'],
+                 ['y', 'z', 'x', 'e', 'q', 's', 't', 'm']]
+    return simpleDat
+
+def createInitSet(dataSet):
+    retDict = {}
+    for i in range(len(dataSet)):
+        retDict[frozenset(dataSet[i])] = 1
+    return retDict
+
+def updateHeader(nodeToLink, newNode):
+    """
+    将node添加到nodeToLink的链表最后
+    :param nodeToLink: 节点链表
+    :param newNode: 新节点
+    :return:
+    """
+    while nodeToLink.nodeLink != None:
+        nodeToLink = nodeToLink.nodeLink
+    nodeToLink.nodeLink = newNode
+
+def updateTree(orderedItems, retTree, headerTable, count):
+    """
+    将某次事务项集排序好后，添加到retTree树中去，该函数是一个递归函数，每次函数调用只添加一个元素到树中，所以树像是逐渐生长一样
+    :param orderedItems: 排好序的某次事务项集
+    :param retTree: 当前FP-Growth树
+    :param headerTable: header table，如createTree函数中解释
+    :param count: 该事务项集在整个数据集中出现的次数
+    :return:
+    """
+    if orderedItems[0] in retTree.children:
+        retTree.children[orderedItems[0]].inc(count)                        # 如果该元素已经存在，则增加该节点的计数
+    else:
+        retTree.children[orderedItems[0]] = treeNode(\
+            orderedItems[0], count, parentNode=retTree)                     # 如果该元素不存在，则生成新的节点添加到当前树中
+        if headerTable[orderedItems[0]][1] == None:
+            headerTable[orderedItems[0]][1] =\
+                retTree.children[orderedItems[0]]                           # 如果该元素对应的header table中的元素尚且没有连接任何节点，则认为该节点是生长出来的第一个节点，则连接此节点
+        else:
+            updateHeader(headerTable[orderedItems[0]][1],\
+                         retTree.children[orderedItems[0]])                 # 如果该元素对应的header table中的元素已经连接节点，则将节点连接到header table中的节点链表的最后
+    if len(orderedItems) > 1:
+        updateTree(orderedItems[1:],\
+                   headerTable[orderedItems[0]][1], headerTable, count)     # 递归生长下一个节点到当前树的子树中
+
+
+def createTree(dataSet, minSup=1):
+    """
+    根据数据集合以及最小支持度来构建FP-Growth树
+    :param dataSet: 数据集字典，形式如下：
+    {frozenset(['a', 'b', 'c']): 1, frozenset(['b', 'c', d]): 1, ...}
+    values代表每个项集出现的次数
+    :param minSup: 最小支持度，小于该最小支持度的项集应该被舍去
+    :return: retTree: FP-Growth树
+    :return: headerTable: 头指针表，header table中存放了每个元素，以及该元素在数据集合中出现的次数，并且连接该元素的在FP-Growth树
+    中的第一个该元素节点，形如：headerTable: {'r': [3, None], 'p': [2, None], 'z': [5, None], 'y': [3, None]}
+    """
+    #--Begin: 第一次循环生成header table
+    headerTable = {}
+    for trans in dataSet.keys():                                            # 循环计算所有元素出现的次数，并记录到headerTable中
+        for item in trans:
+            headerTable[item] = headerTable.get(item, 0) + dataSet[trans]
+
+    for k in list(headerTable.keys()):
+        if headerTable.get(k, 0) < minSup:
+            del(headerTable[k])                                             # 如果headerTable中某元素出现的次数小于最小支持度，则删除该元素
+
+    if len(headerTable) == 0:
+        return None, None                                                   # 如果header table不存在元素，则返回None
+
+    for k in headerTable:
+        headerTable[k] = [headerTable[k], None]                             # headerTable连接了该元素的在FP-Growth树中的第一个该元素节点
+    print("headerTable:", str(headerTable))
+    #--End: 第一次循环生成header table
+
+    #--Begin: 第二次循环，构建FP-Growth树
+    retTree = treeNode('Null set', 1, None)                                 # 创建根节点
+    for tranSet, count in dataSet.items():
+        localD = {}
+        for item in tranSet:
+            if item in set(headerTable.keys()):
+                localD[item] = headerTable[item][0]                         # 记录当前事务集合中每个元素出现的次数，从header table中获取
+        if len(localD) > 0:
+            orderedItems = [v[0] for v in sorted(localD.items(),\
+                                 key=lambda p: p[1], reverse=True)]         # 根据每个元素出现的次数进行排序，次数越多越靠前
+            print("orderedItems:", str(orderedItems))
+            updateTree(orderedItems, retTree, headerTable, count)           # 将本次排好序的项集，构建到FP-Growth树中去
+    return retTree, headerTable
+
+    #--End: 第二次循环，构建FP-Growth树
+
+if __name__ == '__main__':
+    # rootNode = treeNode('a', 1, None)
+    # rootNode.children['b'] = treeNode('b', 2, None)
+    # rootNode.disp(1)
+
+    dataSet = loadSimpDat()
+    dataDic = createInitSet(dataSet)
+    print("dataDic:", str(dataDic))
+    fpTree, headerTable = createTree(dataDic, 3)
+    print("fpTree:")
+    fpTree.disp()
+    print("headerTable:", str(headerTable))
