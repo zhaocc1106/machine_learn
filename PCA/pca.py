1448e5c4f38d619264b51dff4d27e710c9eff484
zhaochaochao <zhaochaochao@baidu.com>
2018-07-21 18:17:18 +0800

    complete pca

--- PCA/pca.py
+++ PCA/pca.py
@@ -26,14 +26,15 @@ def loadDataSet(fileName, delim = '\t'):
     return mat(dataSet)
 
 def pca(dataMat, topNfeat):
-    """
+    """将原始数据进行投影降维基变化
 
     Args:
-        dataMat:
-        topNfeat:
+        dataMat: 原始数据
+        topNfeat: 需要降到的维数
 
     Returns:
-
+        projDataMat: 去中心化后的原始数据对应的降维的数据
+        reconDat: 原始数据降维后所对应的原始坐标
     """
     meanVals = mean(dataMat, axis=0)                            # 求解均值
     meanRemoved = dataMat - meanVals                            # 去中心化
@@ -66,7 +67,48 @@ def showData(dataMat, reconDataMat):
     ax.scatter(reconDataMat[:, 0].T.A[0], reconDataMat[:, 1].T.A[0], marker='o', s=10, c='red')
     plt.show()
 
+def replaceNanWithMean(dataMat):
+    """将原始数据中nan值转换成均值
+
+    Args:
+        dataMat: 原始数据
+
+    Returns:
+        去除掉nan值得数据
+    """
+    numFeat = shape(dataMat)[1]
+    for i in range(numFeat):
+        meanVal = mean(dataMat[nonzero(~isnan(dataMat[:, i].A))[0], i])
+        dataMat[nonzero(isnan(dataMat[:, i].A))[0], i] = meanVal
+    return dataMat
+
 if __name__ == '__main__':
-    dataMat = loadDataSet("testSet.txt")
-    projMat, reconMat = pca(dataMat, 1)
-    showData(dataMat, reconMat)
+    # 使用testSet测试pca
+    # dataMat = loadDataSet("testSet.txt")
+    # projMat, reconMat = pca(dataMat, 1)
+    # showData(dataMat, reconMat)
+
+    # 使用secom.data测试pca
+    dataMat = loadDataSet("secom.data", " ")
+    dataMat = replaceNanWithMean(dataMat)
+    print("dataMat:\n", str(dataMat))
+    varVal = var(dataMat, axis=0)                                               # 求解每个特征的方差
+    # print("varVal:\n", str(varVal))
+    sortVarInd = argsort(varVal[0]).A[0].tolist()                               # 对方差进行排序
+    sortVarInd = sortVarInd[: -21: -1]                                          # 从大到小找出20个方差
+    print("sortVarInd:", str(sortVarInd), "type:", type(sortVarInd))
+    varSum = sum(varVal[0], axis=1)[0, 0]                                       # 求解总方差
+    print("varSum:\n", str(varSum))
+    n = shape(dataMat)[1]
+    print("n:\n", n)
+    sumTmp = 0.0
+    for i in range(20):                                                         # 循环累计20个方差
+        sumTmp += varVal[0, sortVarInd[i]]
+        print("ind:", sortVarInd[i], "var:", str(varVal[0, sortVarInd[i]]))
+        perc = sumTmp / varSum
+        print("i:", i, "perc:", str(perc))                                      # 求累计后方差占总方差的大小
+
+    # 通过上述求解能够知道，前六个方差的累计已经超过了95%，所以设topNfeat为6
+    # 只留6个主成分
+    projMat, reconMat = pca(dataMat, 6)
+    print("projMat:\n", str(projMat), "\nreconMat:\n", str(reconMat))
