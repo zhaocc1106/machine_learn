9d9d213bdb3bd9f03b330cff72ac670edcbc3e22
zhaochaochao <zhaochaochao@baidu.com>
2018-10-29 10:33:40 +0800

    Add auto encoder and multi layer perceptron.

---
+++ tensorflow/MLP.py
@@ -0,0 +1,214 @@
+#
+# Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
+#
+"""
+The simple multi layer perceptron algorithm.
+
+Authors: zhaochaochao(zhaochaochao@baidu.com)
+Date:    2018/10/28 16:40
+"""
+
+# 3rd-part lib
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class Network(object):
+    """
+    Main class to construct and train network.
+    """
+
+    def __init__(self, sizes, drop_keep_p, mini_batch):
+        """The construct function of network.
+
+        :param sizes: The width of every layer.
+        :drop_keep_p: The training dropout probability of hidden layer.
+        :param mini_batch: The mini batch.
+        """
+        self.drop_keep_p = tf.placeholder(tf.float32)
+        self.training_drop_keep_p = drop_keep_p
+        self.mini_batch = mini_batch
+
+        # Initialize all params.
+        self.params = dict()
+        self.params['w'] = []
+        self.params['b'] = []
+        # Firstly, initialize the hidden layer params.
+        for i in range(1, len(sizes) - 1):
+            self.params['w'].append(tf.Variable(
+                tf.truncated_normal([sizes[i - 1], sizes[i]],
+                                    stddev=(1.0 / tf.sqrt(tf.cast(sizes[i],
+                                                                  tf.float32)))
+                                    )))
+            self.params['b'].append(tf.Variable(
+                tf.zeros([sizes[i]], dtype=tf.float32)
+            ))
+        # Secondly, initialize the softmax layer params.
+        self.params['w'].append(tf.Variable(tf.zeros([sizes[-2], sizes[-1]],
+                                                     dtype=tf.float32)))
+        self.params['b'].append(tf.Variable(tf.zeros([sizes[-1]],
+                                                     dtype=tf.float32)))
+
+        print("self.params:")
+        print(self.params)
+
+        # Define the input and desired output. The input is a n * 784 vector.
+        # The output is a n * 10 vector.
+        self.x = tf.placeholder(tf.float32, [None, sizes[0]])
+        self.y_ = tf.placeholder(tf.float32, [None, sizes[-1]])
+
+        # Define the really output.
+        # Firstly, define the every hidden layer output.
+        output_pre_layer = self.x
+        for i in range(1, len(sizes) - 1):
+            output_pre_layer = tf.nn.relu(
+                tf.matmul(output_pre_layer,
+                          self.params['w'][i - 1]) + self.params['b'][i - 1])
+            # Dropout.
+            output_pre_layer = tf.nn.dropout(output_pre_layer,
+                                             self.drop_keep_p)
+        # Secondly, define the softmax layer output.
+        self.y = tf.nn.softmax(tf.matmul(output_pre_layer, self.params['w'][
+            -1]) + self.params['b'][-1])
+
+        # Define the cost use cross entropy.
+        self.cost = tf.reduce_mean(-tf.reduce_sum(self.y_ * tf.log(self.y),
+                                                  reduction_indices=[1]))
+
+    def SGD(self, training_data, eta, epoches, validation_data, test_data):
+        """Training the network using stochastic gradient descent.
+
+        :param training_data: The training data.
+        :param eta: The learning rate.
+        :param epoches: The epoch.
+        :param test_data: The test data.
+        :param validation_data: The validation data.
+        :param test_data: The test data.
+        """
+        # Define the train step.
+        train_step = tf.train.GradientDescentOptimizer(eta).minimize(self.cost)
+
+        # Define the accuracy.
+        correct_prediction = tf.equal(tf.argmax(self.y, axis=1), tf.argmax(
+            self.y_, axis=1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        # Initialize the tensor flow global params.
+        tf.global_variables_initializer().run()
+
+        # Calc the batches number of training, validation and test data.
+        num_train_mini_batches = (int)(training_data.images.shape[0] / \
+                                       self.mini_batch)
+        num_validation_mini_batches = (int)(validation_data.images.shape[0]
+                                            / \
+                                            self.mini_batch)
+        num_test_mini_batches = (int)(test_data.images.shape[0] / \
+                                      self.mini_batch)
+
+        print("num_train_mini_batches:")
+        print(num_train_mini_batches)
+        print("num_validation_mini_batches:")
+        print(num_validation_mini_batches)
+        print("num_test_mini_batches:")
+        print(num_test_mini_batches)
+
+        best_validation_accuracy = 0.0
+        training_accuracys = []
+        validation_accuracys = []
+        for epoch in range(epoches):
+            print("epoch {0} begin##########################".format(epoch))
+            for i in range(num_train_mini_batches):
+                batch_xs, batch_ys = training_data.next_batch(self.mini_batch)
+                train_step.run({self.x: batch_xs,
+                                self.y_: batch_ys,
+                                self.drop_keep_p: self.training_drop_keep_p})
+
+            # Calc the training accuracy.
+            training_accuracy = accuracy.eval({self.x:
+                                                   training_data.images,
+                                               self.y_:
+                                                   training_data.labels,
+                                               self.drop_keep_p:
+                                                   1.0})
+            # Calc the validation accuracy.
+            validation_accuracy = accuracy.eval({self.x:
+                                                     validation_data.images,
+                                                 self.y_:
+                                                     validation_data.labels,
+                                                 self.drop_keep_p:
+                                                     1.0})
+            training_accuracys.append(training_accuracy)
+            validation_accuracys.append(validation_accuracy)
+            print("Epoch {0}, training_accuracy: {1:.2%}, "
+                  "validation_accuracy: {2:.2%}".format(epoch,
+                                                        training_accuracy,
+                                                        validation_accuracy))
+            if validation_accuracy > best_validation_accuracy:
+                best_validation_accuracy = validation_accuracy
+                # Calc the test accuracy.
+                test_accuracy = accuracy.eval({self.x: test_data.images,
+                                               self.y_: test_data.labels,
+                                               self.drop_keep_p: 1.0})
+                print("Get better validation_accuracy, test_accuracy:{"
+                      "0:.2%}".format(test_accuracy))
+        print("Training finished, the best validation accuracy: {0:.2%} with "
+              "test accuracy: {1:.2%}".format(best_validation_accuracy,
+                                              test_accuracy))
+        return training_accuracys, validation_accuracys, test_accuracy
+
+
+def plot_accuracy(training_accuracy, evaluation_accuracy, best_test_accuracy):
+    """Plot the accuracy change of all epochs.
+
+    Args:
+        training_accuracy: The training accuracy list.
+        evaluation_accuracy: The evaluation accuracy list.
+        best_test_accuracy: The best test accuracy.
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    x = np.arange(0, len(training_accuracy), 1)
+    ax.plot(x, training_accuracy, label='train accuracy')
+    ax.plot(x, evaluation_accuracy, label='validation accuracy')
+    plt.xlabel("epoch")
+    plt.ylabel("accuracy")
+    plt.title("The best test accuracy:{0:.2%}".format(best_test_accuracy))
+    plt.legend()
+    plt.show()
+
+
+def load_mnist_data():
+    """Load mnist data.
+
+    :return: mnist data.
+    """
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    return mnist.train, mnist.validation, mnist.test
+
+
+if __name__ == "__main__":
+    training_data, validation_data, test_data = load_mnist_data()
+    print("training_data images shape:")
+    print(training_data.images.shape)
+    print("training_data labels shape:")
+    print(training_data.labels.shape)
+    print("validation_data images shape:")
+    print(validation_data.images.shape)
+    print("validation_data labels shape:")
+    print(validation_data.labels.shape)
+    print("test_data images shape:")
+    print(test_data.images.shape)
+    print("test_data labels shape:")
+    print(test_data.labels.shape)
+
+    sess = tf.InteractiveSession()  # register default session.
+    network = Network(sizes=[784, 300, 10], drop_keep_p=1.0, mini_batch=10)
+    training_accuracys, validation_accuracys, test_accuracy = network.SGD(
+        training_data=training_data,
+        eta=0.01,
+        epoches=50,
+        validation_data=validation_data,
+        test_data=test_data)
+    plot_accuracy(training_accuracys, validation_accuracys, test_accuracy)
---
+++ tensorflow/auto_encoder.py
@@ -0,0 +1,212 @@
+#
+# Copyright (c) 2018 Baidu.com, Inc. All Rights Reserved
+#
+"""
+Construct the auto encoder including gaussian-noise encoder, noiseless encoder
+and random mask noise encoder.
+
+Authors: zhaochaochao(zhaochaochao@baidu.com)
+Date:    2018/10/26 9:22
+"""
+
+# The 3rd part libs
+import numpy as np
+import sklearn.preprocessing as prep
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+
+def xavier_init(fan_in, fan_out, constant=1):
+    """The xavier init function used to initialize the params.
+
+    :param fan_in: The number of input.
+    :param fan_out: The number of output.
+    :param constant: The const.
+    :return: The xavier initialization of params.
+    """
+    low = -constant * np.sqrt(6.0 / (fan_in + fan_out))
+    high = -low
+    # use uniform distribution.
+    return tf.random_uniform((fan_in, fan_out), minval=low, maxval=high,
+                             dtype=tf.float32)
+
+
+class AdditiveGaussianNoiseAutoEncoder(object):
+    """The additive gaussian noise auto encoder.
+
+    Attributes:
+        n_input: The number of inputs.
+        n_hidden: The number of hidden layers.
+        act_func: The activation function.
+        optimizer: The optimizer.
+        scale: The gaussian noise param.
+    """
+
+    def __init__(self, n_input, n_hidden, act_func=tf.nn.softplus,
+                 optimizer=tf.train.AdamOptimizer(), scale=0.1):
+        """The constructor function of additive gaussian noise auto encoder."""
+        self.n_input = n_input
+        self.n_hidden = n_hidden
+        self.act_func = act_func
+        self.optimizer = optimizer
+        self.scale = tf.placeholder(tf.float32)
+        self.training_scale = scale
+        network_weights = self.__initialize_weights()
+        self.weights = network_weights
+
+        # Define the input layer.
+        self.x = tf.placeholder(tf.float32)
+        # Define the hidden layer.
+        self.hidden_layer = self.act_func(tf.add(
+            tf.matmul(self.x + scale * tf.random_normal((n_input,)),
+                      self.weights['w1']), self.weights['b1']))
+        # Define the output layer.
+        self.reconstruction = tf.add(tf.matmul(self.hidden_layer,
+                                               self.weights['w2']),
+                                     self.weights['b2'])
+        # Define the cost.
+        self.cost = 0.5 * tf.reduce_sum(tf.pow(tf.subtract(self.reconstruction,
+                                                     self.x), 2.0))
+        # Define the optimizer.
+        self.optimizer = optimizer.minimize(self.cost)
+        # Init the tf global variables.
+        init = tf.global_variables_initializer()
+        self.session = tf.Session()
+        self.session.run(init)
+
+    def __initialize_weights(self):
+        """Initialize all weights.
+
+        :return: The initial weights.
+        """
+        all_weights = dict()
+        all_weights['w1'] = tf.Variable(
+            xavier_init(self.n_input, self.n_hidden))
+        all_weights['b1'] = tf.Variable(tf.zeros([self.n_hidden],
+                                                 dtype=tf.float32))
+        all_weights['w2'] = tf.Variable(tf.zeros([self.n_hidden, self.n_input],
+                                                 dtype=tf.float32))
+        all_weights['b2'] = tf.Variable(tf.zeros([self.n_input],
+                                                 dtype=tf.float32))
+        return all_weights
+
+    def train(self, x_train, x_test, epoches=20, batch_size=128,
+              display_setp=1):
+        """Train the network by inputs.
+
+        :param x_train: The training x.
+        :param x_test: The test x.
+        :param epoches: The epoches.
+        :param batch_size: The batch size.
+        :param display_setp: The step of display message.
+
+        :return:
+            min_cost: The minimum cost.
+        """
+
+        def partial_fit(x):
+            """The training function."""
+            cost, opt = self.session.run((self.cost, self.optimizer),
+                                         feed_dict={self.x: x, self.scale:
+                                             self.training_scale})
+            return cost
+
+        train_batches = int(x_train.shape[0] / batch_size)
+
+        for epoch in range(epoches):
+            avg_cost = 0.0
+            for i in range(train_batches):
+                x_batch = self.get_random_block_from_data(x_train, batch_size)
+                cost = partial_fit(x_batch)
+                avg_cost += cost / train_batches
+            test_cost = partial_fit(x_test)
+            if epoch % display_setp == 0:
+                print("The epoch({0}): train average cost:{1:.9f}, test cost:{"
+                      "2:.9f}".format(epoch, avg_cost, test_cost))
+
+    def get_random_block_from_data(self, x, batch_size):
+        """Return random mini batch.
+
+        :param x: The inputs
+        :param batch_size: The mini batch size.
+        :return: The mini batch.
+        """
+        start_index = np.random.randint(0, x.shape[0] - batch_size)
+        return x[start_index: start_index + batch_size]
+
+    def calc_total_cost(self, x):
+        """Calculate the total cost.
+
+        :param x: The inputs.
+        :return: The total cost.
+        """
+        return self.session.run(self.cost, feed_dict={self.x: x, self.scale:
+            self.training_scale})
+
+    def transform(self, x):
+        """Transform the inputs to high dimension characteristic.
+
+        :param x: The inputs.
+        :return: The high dimension characteristic.
+        """
+        return self.session.run(self.hidden_layer,
+                                feed_dict={self.x: x, self.scale:
+                                    self.training_scale})
+
+    def generate(self, hidden_layer):
+        """Generate the original data by hidden layer.
+
+        :param hidden_layer: The hidden layer.
+        :return: The original inputs data.
+        """
+        return self.session.run(self.reconstruction,
+                                feed_dict={self.hidden_layer: hidden_layer})
+
+    def reconstruct(self, x):
+        """The combination of transform with generate.
+
+        :param x: The inputs.
+        :return: The inputs reconstructed.
+        """
+        return self.session.run(self.reconstruction,
+                                feed_dict={self.x: x, self.scale:
+                                    self.training_scale})
+
+    def standard_data(self, x_train, x_test):
+        """Standard the training data and test data. If the average of data
+        is u and standard deviation of data is o, the standard data is (x -
+        u) / o.
+
+        :param x_train: The training data.
+        :param x_test: The test data.
+        :return: The standard training data and test data.
+        """
+        preprocessor = prep.StandardScaler().fit(x_train)
+        x_train = preprocessor.transform(x_train)
+        x_test = preprocessor.transform(x_test)
+        return x_train, x_test
+
+if __name__=="__main__":
+    autoEncoder = AdditiveGaussianNoiseAutoEncoder(
+        n_input=784,
+        n_hidden=200,
+        act_func=tf.nn.softplus,
+        optimizer=tf.train.AdamOptimizer(learning_rate=0.001),
+        scale=0.01
+    )
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    x_train, x_test = autoEncoder.standard_data(mnist.train.images,
+                                                mnist.test.images)
+    print("x_train shape:")
+    print(x_train.shape)
+    print("x_test shape:")
+    print(x_test.shape)
+    # x_train, x_test = mnist.train.images, mnist.test.images
+    autoEncoder.train(
+        x_train=x_train,
+        x_test=x_test,
+        epoches=20,
+        batch_size=128,
+        display_setp=1
+    )
+
