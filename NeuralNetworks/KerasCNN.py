591c16fa595b4fdb7e608498f245585b55d76d98
zhaochaochao <zhaochaochao@baidu.com>
2019-06-16 17:51:52 +0800

    Add prediction.
    Change-Id: Ifda5a47cfe5ea3e9a8776a4aaaf5768d5e418409

--- NeuralNetworks/KerasCNN.py
+++ NeuralNetworks/KerasCNN.py
@@ -15,17 +15,14 @@ import shutil
 
 # 3rd-part libs.
 from numpy import *
-import keras
-from keras.datasets import mnist
-from keras.layers import Dense, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras.models import Sequential
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 MODEL_PATH = "/tmp/kerasCNN/"
-SAVER_PATH = "/tmp/kerasCNN/model_saver"
+SAVER_PATH = "/tmp/kerasCNN/model_saver.h5"
 
 
-def trainKerasCNN(trainX, trainY, testX, testY):
+def train_keras_cnn(trainX, trainY, testX, testY):
     """使用keras构建CNN训练手写识别系统
 
     Args:
@@ -37,29 +34,36 @@ def trainKerasCNN(trainX, trainY, testX, testY):
     Returns:
         errRate: 错误率
     """
-    batchSize = 10
+    batch_size = 10
     epochs = 10
-    numClass = 10  # 手写识别有10个数字
+    # 手写识别有10个数字
+    num_class = 10
 
-    # convert class vectors to binary class matrices - this is for use in the
-    # categorical_crossentropy loss below
-    trainY = keras.utils.to_categorical(trainY, numClass)
-    testY = keras.utils.to_categorical(testY, numClass)
-    # print("trainY:\n", str(trainY), "testY:\n", str(testY))
+    # 将类型label转换为二进制表示，比如1转换为(1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    trainY = tf.keras.utils.to_categorical(trainY, num_class)
+    testY = tf.keras.utils.to_categorical(testY, num_class)
 
-    model = Sequential()  # 创建序贯模型
-    model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
-                     activation='relu', input_shape=[28, 28, 1]
-                     ))  # 添加一个卷积层, 32个卷积核，激活函数用relu
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))  # 添加一个max pool层
-    model.add(Conv2D(64, (5, 5), activation="relu"))  # 添加第二个卷积层
-    model.add(MaxPooling2D(pool_size=(2, 2)))  # 添加第二个max pool层
-    model.add(Flatten())  # 添加flatten层
-    model.add(Dense(1000, activation="relu"))  # 添加完全连接层，1000个nn，使用relu激活函数
-    model.add(Dense(numClass, activation="softmax"))  # 添加完全连接层作为输出层，分成10个类
+    # 创建 keras 序贯模型
+    model = tf.keras.models.Sequential()
+    # 添加一个卷积层, 32个卷积核，激活函数用relu
+    model.add(tf.keras.layers.Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
+                                     activation='relu', input_shape=[28, 28, 1]
+                                     ))
+    # 添加一个max pool层
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    # 添加第二个卷积层
+    model.add(tf.keras.layers.Conv2D(64, (5, 5), activation="relu"))
+    # 添加第二个max pool层
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    # 添加flatten层
+    model.add(tf.keras.layers.Flatten())
+    # 添加完全连接层，1000个nn，使用relu激活函数
+    model.add(tf.keras.layers.Dense(1000, activation="relu"))
+    # 添加完全连接层作为输出层，分成10个类
+    model.add(tf.keras.layers.Dense(num_class, activation="softmax"))
 
-    model.compile(loss=keras.losses.categorical_crossentropy,  # 标准交叉熵来进行分类
-                  optimizer=keras.optimizers.Adam(),  # 使用Adam优化器
+    model.compile(loss=tf.keras.losses.categorical_crossentropy,  # 使用交叉熵损失函数
+                  optimizer=tf.keras.optimizers.Adam(),  # 使用Adam优化器
                   metrics=['accuracy'])  # 在训练和测试时需要评估的度量
 
     def learning_rate_scheduler(epoch):
@@ -76,78 +80,105 @@ def trainKerasCNN(trainX, trainY, testX, testY):
 
     callbacks = [
         # Model saver callback.
-        keras.callbacks.ModelCheckpoint(filepath=SAVER_PATH),
-
+        tf.keras.callbacks.ModelCheckpoint(filepath=SAVER_PATH),
         # Learning rate scheduler.
-        keras.callbacks.LearningRateScheduler(learning_rate_scheduler,
-                                              verbose=1),
-
+        tf.keras.callbacks.LearningRateScheduler(learning_rate_scheduler,
+                                                 verbose=1),
         # Tensorboard callback.
-        keras.callbacks.TensorBoard(log_dir=MODEL_PATH)
+        tf.keras.callbacks.TensorBoard(log_dir=MODEL_PATH)
     ]
-
     if os.path.exists(MODEL_PATH):
         shutil.rmtree(MODEL_PATH)
 
     model.fit(trainX,  # 输入数据列表
               trainY,  # 输入标签列表
-              batch_size=batchSize,  # 梯度更新时样本数
+              batch_size=batch_size,  # 梯度更新时样本数
               epochs=epochs,  # 训练轮数
               verbose=1,  # log等级
               validation_data=(testX, testY),  # 测试数据与标签
               callbacks=callbacks  # 设置一些回调
               )
-
     """
     # 使用dataset 数据集作为输入，使用fit_generator训练模型
     dataset = tf.data.Dataset.from_tensor_slices((trainX, trainY))
-    dataset = dataset.batch(batchSize)
+    dataset = dataset.batch(batch_size)
     dataset = dataset.repeat()
     iterator = dataset.make_initializable_iterator()
     next_element = iterator.get_next()
     sess = tf.InteractiveSession()
     sess.run(iterator.initializer)
-
     def _input_func(sess, next_element):
         while True:
             val = sess.run(next_element)
             yield val
-
-    steps_per_epoch = trainX.shape[0] / batchSize
+    steps_per_epoch = trainX.shape[0] / batch_size
     model.fit_generator(_input_func(sess, next_element),
                         epochs=epochs,
                         steps_per_epoch=steps_per_epoch,
                         verbose=1,
                         validation_data=(testX, testY))
     """
-
     score = model.evaluate(testX, testY, verbose=0)  # 评估模型
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
 
 
+def predict(testX):
+    """预测输入图片的类型
+
+    Args:
+        testX: The test data features.
+    """
+    # 加载整个模型，包括 graphs 和 weights
+    model = tf.keras.models.load_model(SAVER_PATH)
+
+    labels = model.predict(testX)
+    print(labels)
+
+    # 显示分类结果
+    n = testX.shape[0]
+    nc = int(ceil(n / 4))
+    f, axes = plt.subplots(nc, 4)
+    for i in range(nc * 4):
+        x = i // 4
+        y = i % 4
+        axes[x, y].axis('off')
+
+        label = argmax(labels[i])
+        confidence = max(labels[i])
+        if i > n:
+            break
+        axes[x, y].imshow(reshape(testX[i] * 255, (28, 28)))
+        axes[x, y].text(0.5, -1.0, str(label) + "\n%.3f" %
+                        confidence, fontsize=14)
+    plt.show()
+
+
 if __name__ == "__main__":
-    # input image dimensions
-    imgX, imgY = 28, 28
+    # 图片宽和高
+    img_h, img_w = 28, 28
 
-    (trainX, trainY), (testX, testY) = mnist.load_data()
-    # trainX, trainY = Utils.loadData("trainingDigits")
-    # testX, testY = Utils.loadData("testDigits")
+    # 加载手写识别数据集，其中X代表图片数据，Y代表label
+    (trainX, trainY), (testX, testY) = tf.keras.datasets.mnist.load_data()
 
-    # reshape the data into a 4D tensor - (sample_number, x_img_size,
-    # y_img_size, num_channels)
-    # because the MNIST is greyscale, we only have a single channel - RGB
-    # colour images would have 3
-    trainX = array(trainX).reshape(trainX.shape[0], imgX, imgY, 1)
-    testX = testX.reshape(testX.shape[0], imgX, imgY, 1)
-    input_shape = (imgX, imgY, 1)
+    # 将图片数据转换为CNN模型输入的shape
+    trainX = array(trainX).reshape(trainX.shape[0], img_h, img_w, 1)
+    testX = testX.reshape(testX.shape[0], img_h, img_w, 1)
 
-    # convert the data to the right type
+    # 转换图片数据的数据类型
     trainX = trainX.astype('float32')
     testX = testX.astype('float32')
-    trainX /= 255
-    testX /= 255
+
+    # 将数据缩小到0-1范围内
+    trainX = trainX / 255
+    testX = testX / 255
+
     print('trainX shape:', trainX.shape)
     print(trainX.shape[0], 'trainX samples')
     print(testX.shape[0], 'testX samples')
-    trainKerasCNN(trainX, trainY, testX, testY)
+
+    # 构建并训练分类手写识别数据的cnn模型
+    train_keras_cnn(trainX, trainY, testX, testY)
+
+    # 预测测试数据中前8个图片的类型
+    predict(testX[: 8])
